93fa3aae5fcbda388d24018bf409abcceeb3c139
Rahul Bhardwaj <rajputrahul445@gmail.com>
2024-11-06 23:25:45 +0530

    webpack changes

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+var webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = 'style-loader';
+
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        // new HtmlWebpackPlugin({
+        //     template: 'index.html',
+        // }),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            // publicPath: '/immigration'
+        }),
+        new CopyPlugin({
+            patterns: [
+                // { from: './src/index.html' },
+                { from: './package.json' },
+                { from: './src/App.css', to: 'css'},
+                { from: './public/images', to: 'images'}
+            ]
+        })
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
